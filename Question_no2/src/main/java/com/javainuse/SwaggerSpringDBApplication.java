57c3e3f93b7faa60523d6d3f276d8e43b4a31bb4
AtishayJainGL <110605732+AtishayJainGL@users.noreply.github.com>
2022-09-05 10:55:37 +0530

    Add files via upload

---
+++ Question_no2/src/main/java/com/javainuse/SwaggerSpringDBApplication.java
@@ -0,0 +1,15 @@
+/*write a program to describe Swagger API documentation.
+  (hint: try to use GET,POST,PUT,DELTE)*/
+package com.javainuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerSpringDBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerSpringDBApplication.class, args);
+	}
+
+}
